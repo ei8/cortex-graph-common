cb7a1080a80137972261114d386a71f285348801
Elmer Roderick Bool <elmer.bool@ei8.works>
2020-02-15 12:14:25 +1100

    Initial commit

---
+++ src/main/Neuron.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace works.ei8.Cortex.Graph.Common
+{
+    public class Neuron
+    {
+        public string Id { get; set; }
+
+        public string Tag { get; set; }
+
+        public Terminal Terminal { get; set; }
+
+        [JsonIgnore]
+        public RelativeType Type => this.Terminal != null && this.Terminal.PresynapticNeuronId != null ?
+            this.Terminal.PresynapticNeuronId.EndsWith(this.Id) ?
+                RelativeType.Presynaptic :
+                RelativeType.Postsynaptic :
+            RelativeType.NotSet;
+
+        public int Version { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string AuthorTag { get; set; }
+
+        public string LayerId { get; set; }
+
+        public string LayerTag { get; set; }
+
+        public string Timestamp { get; set; }
+
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
---
+++ src/main/Terminal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace works.ei8.Cortex.Graph.Common
+{
+    public class Terminal
+    {
+        public string Id { get; set; }
+        public string PresynapticNeuronId { get; set; }
+        public string PostsynapticNeuronId { get; set; }
+        public string Effect { get; set; }
+        public string Strength { get; set; }
+        public int Version { get; set; }
+        public string AuthorId { get; set; }
+        public string AuthorTag { get; set; }
+        public string Timestamp { get; set; }
+    }
+}
