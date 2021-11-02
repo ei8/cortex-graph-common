81ac249068e867aeb238b7668703e2cd55cf23fb
elmerbool <35117496+elmerbool@users.noreply.github.com>
2021-11-02 23:25:12 +1100

    Added query string processing methods; Added parse and build methods to NeuronQuery.

---
+++ src/main/ExtensionMethods.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Web;
+
+namespace ei8.Cortex.Graph.Common
+{
+    public static class ExtensionMethods
+    {
+        public static int? GetNullableIntValue(this IEnumerable<string> queryStrings, string fieldName) =>
+            ExtensionMethods.QueryStringsContains(queryStrings, fieldName) ? 
+                (int?) int.Parse(ExtensionMethods.GetSingleQueryParameter(queryStrings, fieldName)) : 
+                null;
+
+        public static bool? GetNullableBoolValue(this IEnumerable<string> queryStrings, string fieldName) =>
+            ExtensionMethods.QueryStringsContains(queryStrings, fieldName) ?
+                (bool?) bool.Parse(ExtensionMethods.GetSingleQueryParameter(queryStrings, fieldName)) :
+                null;
+
+        public static T? GetNullableEnumValue<T>(this IEnumerable<string> queryStrings, string fieldName) where T : struct, Enum =>
+            ExtensionMethods.QueryStringsContains(queryStrings, fieldName) ? 
+                (T?) Enum.Parse(typeof(T), ExtensionMethods.GetSingleQueryParameter(queryStrings, fieldName), true) : 
+                null;
+
+        private static bool IsQueryParameter(string query, string parameterName) =>
+            query.StartsWith(parameterName + "=", true, null);
+        private static bool QueryStringsContains(IEnumerable<string> queryStrings, string parameterName) =>
+            queryStrings.Any(s => ExtensionMethods.IsQueryParameter(s, parameterName));
+        private static string GetSingleQueryParameter(IEnumerable<string> queryStrings, string parameterName) =>
+            queryStrings.SingleOrDefault(s => ExtensionMethods.IsQueryParameter(s, parameterName))?.Substring(parameterName.Length + 1);
+        private static IEnumerable<string> ConvertQueryParameters(IEnumerable<string> queryStrings, string parameterName) =>
+            queryStrings.Where(s => ExtensionMethods.IsQueryParameter(s, parameterName)).Select(s => HttpUtility.UrlDecode(s.Substring(parameterName.Length + 1)));
+
+        public static IEnumerable<string> GetQueryArrayOrDefault(this IEnumerable<string> queryStrings, string parameterName)
+        {
+            var parameterNameExclamation = parameterName.Replace("Not", "!");
+            var stringArray = ExtensionMethods.QueryStringsContains(queryStrings, parameterName) ?
+                ExtensionMethods.ConvertQueryParameters(queryStrings, parameterName) :
+                ExtensionMethods.QueryStringsContains(queryStrings, parameterNameExclamation) ?
+                    ExtensionMethods.ConvertQueryParameters(queryStrings, parameterNameExclamation) :
+                    null;
+
+            return stringArray != null ? stringArray.Select(s => s != "\0" && s != "%00" ? s : null) : stringArray;
+        }
+
+        public static string GetQueryKey(this Type type, string propertyName)
+        {
+            var pInfo = type.GetProperty(propertyName);
+
+            if (pInfo == null)
+                throw new ArgumentOutOfRangeException($"Specified property with name '{propertyName}' was not found.");
+
+            var qk = pInfo.GetCustomAttribute<QueryKeyAttribute>();
+
+            if (qk == null)
+                throw new ArgumentOutOfRangeException($"Specified property with name '{propertyName}' does not have a QueryKeyAttribute.");
+
+            return !string.IsNullOrWhiteSpace(qk.Value) ? qk.Value : propertyName;
+        }
+    }
+}
---
+++ src/main/QueryKeyAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ei8.Cortex.Graph.Common
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class QueryKeyAttribute : Attribute
+    {
+        public QueryKeyAttribute(string value = default)
+        {
+            this.Value = value;
+        }
+
+        public string Value { get; private set; }
+    }
+}
